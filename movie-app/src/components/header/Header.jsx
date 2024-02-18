c92c94b9998552bab97241fad3d771dc238eca88
shubhambudde1 <shubhambudde.github@gmail.com>
2024-02-18 03:39:56 -0800

    n

--- movie-app/src/components/header/Header.jsx
+++ movie-app/src/components/header/Header.jsx
@@ -18,6 +18,15 @@ const Header = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const searchQueryHandler = (event) => {
+      if (event.key === 'Enter' && query.length > 0) {
+        navigate(`/search/${query}`);
+        setTimeout(() => {
+          setShowSearch(false);
+        }, 1000)
+      }
+    };
+
 
     const openSearch = () => {
       setMobileMenu(false)
@@ -30,7 +39,7 @@ const Header = () => {
     }
 
     return (
-        <header className="header">
+        <header className = {`header ${mobileMenu ? "mobileView" : " "}  ${show}`}>
         <ContentWrapper>
         <div className = "logo">
             <img src={logo} alt="" />
@@ -44,16 +53,30 @@ const Header = () => {
             
           </ul>
           <div className="mobileMenuItem">
-          <HiOutlineSearch />
+          <HiOutlineSearch onClick={openSearch} />
           {mobileMenu ? (
-            <VscChromeClose onClick={() => setMobileMenu(false)} />
+            <VscChromeClose onClick={() => setMobileMenu(false )} />
           ):(
             <SlMenu onClick={opemMobileMenu}/>)}
           
-          <VscChromeClose />
+          
           </div>
 
         </ContentWrapper>
+        {showSearch && <div className="searchBar">
+        <ContentWrapper>
+        <div className='searchInput'>
+            <input
+              type='text'
+              placeholder='search for movie or tv shows...'
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose onClick={() => setMobileMenu(false )} />
+            
+          </div>
+        </ContentWrapper>
+        </div>}
           
         </header>
     );
